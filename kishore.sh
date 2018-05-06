ad8dcaf7100fa73588641307a525e9496706035d
nkishorekumar10 <34569933+nkishorekumar10@users.noreply.github.com>
2018-05-06 17:21:20 +0530

    Add files via upload

---
+++ kishore.sh
@@ -0,0 +1,24 @@
+#created by kishore
+pkg install git
+pkg install clang
+pkg install php
+pkg install python2
+pkg install python
+pkg install ruby
+pkg install figlet
+pkg install toilet
+pkg install perl
+pkg install bash
+apt update
+apt upgrade
+
+#now install apps
+git clone https://github.com/Gameye98/Lazymux
+git clone https://github.com/evait-security/weeman
+git clone https://github.com/grafov/hulk
+git clone https://github.com/zanyarjamal/xerxes
+git clone https://github.com/dotfighter/torshammer
+
+figlet CREATED BY KISHORE | lolcat
+figlet CONTACT ME: nkishorekumar10@gmail.com | lolcat
+toilet script written by kishore |lolcat
